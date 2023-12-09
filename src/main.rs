ae75bc65d958fc5c06dafb5a4362926f13edf501
robielcpnv <robboytm@gmail.com>
2023-12-09 18:22:44 +0100

    :zap: Add variable and function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+fn hello() {
+    let mut my_name = "Robiel";
+    my_name = "Robiel Tesfazghi";
+    println!("Hello, world! My name is {}!", my_name);
+}
+
+fn add () {
+    let x = 5;
+    let y = 10;
+    let sum = x + y;
+    println!("The sum of {} and {} is {}.", x, y, sum);
+}
+
 fn main() {
-    println!("Hello, world!");
+   hello();
+   add()
 }
