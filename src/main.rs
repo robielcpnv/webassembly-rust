8cb1ba91f604e65dde692cd761b627e4275822f4
robielcpnv <robboytm@gmail.com>
2023-12-09 19:18:36 +0100

    :zap: Add match expression

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,37 @@
-fn hello() {
-    let mut my_name = "Robiel";
-    my_name = "Robiel Tesfazghi";
-    println!("Hello, world! My name is {}!", my_name);
-}
-
 fn add (num_one: i32, num_two:i32) -> i32 {
     let sum = num_one + num_two;
-    println!("The sum of {} and {} is {}.", num_one, num_two, sum);
     return sum;
 }
 
 fn main() {
-   hello();
-   add(10,5);
+   let mut total = add(10,15);
+   let mut free_shipping = false;
+
+   if total > 50 {
+         println!("You qualify for free shipping!");
+        free_shipping = true;
+    } 
+    else if total > 20 {
+        println!("If you add more items, you can qualify for free shipping!");
+    }
+    else {
+        println!("No free shipping for you!"); 
+    }
+
+    match total {
+        0 => println!("Your cart is empty!"),
+        1..=50 => println!("Total: {:?}", total),
+        _ => println!("No match found"),
+        
+    }
+
+    total = match free_shipping {
+        true =>  add(total, 0),
+        false => add(total, 5),
+        
+    };
+
+
+
+    println!("Your total is: {:?}", total);
 }
