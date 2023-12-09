4da2ef6c1da7968742037df4dff0962cdc201a60
robielcpnv <robboytm@gmail.com>
2023-12-09 19:53:09 +0100

    :zap: Add array and vector

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 fn add (num_one: i32, num_two:i32) -> i32 {
     let sum = num_one + num_two;
     return sum;
@@ -7,8 +9,8 @@ fn main() {
    let mut total = add(10,15);
    let mut free_shipping = false;
 
-   if total > 50 {
-         println!("You qualify for free shipping!");
+    if total > 50 {
+            println!("You qualify for free shipping!");
         free_shipping = true;
     } 
     else if total > 20 {
@@ -31,7 +33,19 @@ fn main() {
         
     };
 
-
-
     println!("Your total is: {:?}", total);
+
+    let items:[i32;5] = [1,2,3,4,5];
+    println!("Array Items: {:?}", items);
+
+    let vector_items = vec![1,2,3,4,5];
+    let mut vector_items_two = Vec::new();
+    vector_items_two.push(1);
+    vector_items_two.push(2);
+    vector_items_two.push(3);
+    vector_items_two.push(4);
+    vector_items_two.push(5);
+
+    println!("Vector Items: {:?}", vector_items);
+    println!("Vector Items Two: {:?}", vector_items_two);
 }
