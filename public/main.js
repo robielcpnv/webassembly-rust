fbc8beae658d0f84c22c3f41736d048fae060bdd
robielcpnv <robboytm@gmail.com>
2023-12-13 19:30:03 +0100

    :zap: Loggging the js file in rust lib

--- public/main.js
+++ public/main.js
@@ -17,8 +17,7 @@ fileReader.onload = () => {
    const base64 = fileReader.result.replace(
     /^data:image\/(png|jpeg|jpg);base64,/,''
    )
-   console.log(input.files[0]);
-   console.log(base64);
+   rustApp.grayscale(base64)
 }
 
 input.addEventListener('change', () => {
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use wasm_bindgen::prelude::*;
+use web_sys::console::log_1 as log;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[wasm_bindgen]
+pub fn grayscale(encoded_file: &str) {
+    log(&encoded_file.into());
 }
