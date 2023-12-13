48ff060b42087ffe12b3f6dbc4acea1859cc368c
robielcpnv <robboytm@gmail.com>
2023-12-13 18:59:34 +0100

    :zap: Add Reading file function

--- public/main.js
+++ public/main.js
@@ -1,1 +1,19 @@
-console.log('test');
+async function init() {
+const input = document.getElementById('upload')
+const fileReader = new FileReader()
+
+
+fileReader.onload = () => {
+   const base64 = fileReader.result.replace(
+    /^data:image\/(png|jpeg|jpg);base64,/,''
+   )
+   console.log(input.files[0]);
+   console.log(base64);
+}
+
+input.addEventListener('change', () => {
+   fileReader.readAsDataURL(input.files[0])
+})
+}
+
+init();
