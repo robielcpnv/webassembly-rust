9b35afa1545c831897dac935027e173904daabb4
robielcpnv <robboytm@gmail.com>
2023-12-13 19:39:37 +0100

    :zap: Upload image to rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console::log_1 as log;
+use base64::decode;
+use image::load_from_memory;
 
 #[wasm_bindgen]
 pub fn grayscale(encoded_file: &str) {
-    log(&encoded_file.into());
+    log(&"Grayscale called".into());
+    let base64_to_vector = decode(encoded_file).unwrap();
+    log(&"Image Decoded".into());
+
+    let image = load_from_memory(&base64_to_vector).unwrap();
+    log(&"Image Loaded".into());
 }
