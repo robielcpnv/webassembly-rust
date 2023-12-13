99dc2ea11a6136652a26f5cb0c3df1788021cfa8
robielcpnv <robboytm@gmail.com>
2023-12-13 15:53:51 +0100

    :zap: Add Results

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,23 @@ fn print_verified(account: &BankAccount) {
 fn print_owner(account: &BankAccount) {
     println!("Account owner: {:?}", account.owner);
 }
+fn is_verified(account: &BankAccount) -> Result<bool, bool> {
+    match account.verified {
+        true => Ok(true),
+        false => Err(false),
+        
+    }    
+}
 fn main() {
     let my_account = BankAccount {
         balance: 100,
         verified: true,
         owner: String::from("Robiel Tesfazghi"),
     };
+    let verification_status = is_verified(&my_account)
+        .expect("Error checking verification status");
     print_balance(&my_account);
     print_verified(&my_account);
     print_owner(&my_account);
+    println!("Account verification status: {:?}", verification_status);
 }
